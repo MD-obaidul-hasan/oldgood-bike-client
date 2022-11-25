3d390ac64531645bd7a801fc1c0cc68c100ca7f7
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-11-25 12:27:31 +0600

    BookBike data crate

--- src/Pages/Bikecatagory/Availablecata/Availablecata.js
+++ src/Pages/Bikecatagory/Availablecata/Availablecata.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {format} from 'date-fns'
+import BikeBookoption from './BikeBookoption';
 
 const Availablecata = ( {selectedDate}) => {
-    return (
-        <section className='mt-16'>
-            <p className='text-center text-secondary font-bold'>You have selected  {format(selectedDate, 'PP')}
+    const [catagoriesBikes, setCatagoriesbike] = useState([])
 
-            </p>
+    useEffect(() =>{
+        fetch('catagoriesbike.json')
+        .then(res => res.json())
+        .then(data => setCatagoriesbike(data))
+    },[])
+    return (
+        <section className='my-16'>
+            <p className='text-center text-secondary font-bold'>You have selected  {format(selectedDate, 'PP')} </p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    catagoriesBikes.map(option => <BikeBookoption
+                    key={option._id}
+                    catagoriesBike={option}
+                    ></BikeBookoption>)
+                }
+            </div>
         </section>
     );
 };
---
+++ src/Pages/Bikecatagory/Availablecata/BikeBookoption.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const BikeBookoption = ({ catagoriesBike }) => {
+    const { name, location,resaleprice,orginalprice,year_of_use,seller_Name, image, slots } = catagoriesBike;
+    return (
+        <div className="card  shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={image} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="text-2xl text-secondary font-bold text-center">Model:{name}</h2>
+                <h3 className="text-2xl text-primary font-bold text-center">Area:{location}</h3>
+                <h2 className="text-2xl text-warning font-bold text-center">Resale price:{resaleprice}</h2>
+                <h3 className="text-2xl text-accent font-bold text-center">Orginal price:{orginalprice}</h3>
+                <h2 className="text-2xl text-error font-bold text-center">Total use:{year_of_use}</h2>
+                <h2 className="text-2xl text-blue font-bold text-center">Seller:{seller_Name}</h2>
+                <p>{slots.length > 0 ? slots[0] : 'Try Another day'} post available</p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} </p>
+                <div className="card-actions">
+                    <button className="btn btn-primary text-white">Book Bike</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BikeBookoption;
