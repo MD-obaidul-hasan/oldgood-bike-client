0946995aaa92e5205df604715e23e1675a558a09
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-11-27 20:22:53 +0600

    usedata lord

--- src/Pages/Dashboard/Myoderbike/Myoderbike.js
+++ src/Pages/Dashboard/Myoderbike/Myoderbike.js
@@ -1,4 +1,5 @@
 // import {useQuery} from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../Contextx/AuthProvider';
 import useTitle from '../../../hooks/useTitle';
@@ -8,14 +9,14 @@ const Myoderbike = () => {
     const  {user} = useContext(AuthContext);
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
 
-    // const {data: bookings = []} = useQuery({
-    //     queryKey: ['bookings', user?.email],
-    //     queryFn: async () => {
-    //         const res = await fetch(url);
-    //         const data = await res.json();
-    //         return data;
-    //     }
-    // })
+    const {data: bookings = []} = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        }
+    })
 
     return (
         <div>
@@ -25,15 +26,15 @@ const Myoderbike = () => {
                                         <thead>
                         <tr>
                             <th></th>
-                            <th>Name</th>
-                            <th>Bike Model</th>
-                            <th>Date</th>
-                            <th>Time</th>
+                            <th>name</th>
+                            <th>price</th>
+                            <th>item</th>
+                            <th>email</th>
                         </tr>
                     </thead>
                     <tbody>
                         
-                        {/* {
+                        {
                             bookings.map((booking, i) =><tr key={booking._id}>
                                 <th>{i+1}</th>
                                 <td>{booking.name}</td>
@@ -41,7 +42,7 @@ const Myoderbike = () => {
                                 <td>{booking.date}</td>
                                 <td>{booking.slot}</td>
                             </tr>)
-                        } */}
+                        }
                         
                         
                     </tbody>
