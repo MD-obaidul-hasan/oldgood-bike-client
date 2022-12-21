5b375bfc89c7d0ec72a3f6a36e2f8bf62f2f6106
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-12-21 22:46:13 +0600

    blog color

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -31,7 +31,7 @@ const Blog = () => {
                 </div>
             </div>
 
-            <div className="card w-96 bg-cyan-600 text-primary-content">
+            <div className="card w-96 bg-cyan-300 text-primary-content">
                 <div className="card-body">
                     <h2 className="card-title"> How does prototypical inheritance work?</h2>
                     <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
@@ -41,7 +41,7 @@ const Blog = () => {
                 </div>
              </div>
 
-            <div className="card w-96 bg-orange-600 text-primary-content">
+            <div className="card w-96 bg-orange-300 text-primary-content">
                 <div className="card-body">
                     <h2 className="card-title"> What is a unit test? Why should we write unit tests?</h2>
                     <p>The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.</p>
@@ -50,7 +50,7 @@ const Blog = () => {
                 </div>
             </div>
 
-            <div className="card w-96 bg-yellow-600 text-primary-content">
+            <div className="card w-96 bg-yellow-300 text-primary-content">
                 <div className="card-body">
                     <h2 className="card-title">React vs. Angular vs. Vue?</h2>
                     <p>Vue provides higher customizability and hence is easier to learn than Angular or React. Further, Vue has an overlap with Angular and React with respect to their functionality like the use of components. Hence, the transition to Vue from either of the two is an easy option</p>
